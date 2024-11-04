7ffaffb17eb2c813b5225a0e7d31f86c76fc93ca
Kyukyoung Song (James Song) <skdmlrh12@gmail.com>
2024-11-04 20:52:13 +0900

    Feat: Prettier 및 Husky 세팅

--- app.js
+++ app.js
@@ -1,40 +1,40 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const app = express();
 
-require('dotenv').config();
+require("dotenv").config();
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 const username = encodeURIComponent(process.env.DB_USERNAME);
@@ -42,7 +42,7 @@ const password = encodeURIComponent(process.env.DB_PASSWORD);
 
 const uri = `mongodb+srv://${username}:${password}@bloblow.naudn.mongodb.net/?retryWrites=true&w=majority&appName=bloblow`;
 
-const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
+const clientOptions = { serverApi: { version: "1", strict: true, deprecationErrors: true } };
 
 async function run() {
   try {
