bc6171d62518f4fd7f24be16f4a0ed691d7ca666
Jintae Song <w.jinttao@gmail.com>
2024-11-05 18:16:40 +0900

    Fix: groupRoute 경로 중복 설정으로 발생한 404 Not Found 이슈를 상위 경로로 수정하여 해결

--- routes/groupRoute.js
+++ routes/groupRoute.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const groupController = require("../controllers/groupController");
 
-router.post("/group", groupController.create);
+router.post("/", groupController.create);
 
 module.exports = router;
