aa966fd74e5bbbdb8bfd641fdaa11ea578deb24f
Jintae Song <w.jinttao@gmail.com>
2024-11-05 18:08:04 +0900

    Refactor: group schema 포맷 수정

--- models/groupModel.js
+++ models/groupModel.js
@@ -1,22 +1,24 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
-const groupSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
+const groupSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    ownerId: {
+      type: String,
+      required: true,
+    },
+    keywordList: {
+      type: Array,
+      required: true,
+    },
   },
-  ownerId: {
-    type: String,
-    required: true,
-  },
-  keywordList: {
-    type: Array,
-    required: true,
-  },
-},
-{
-  timestamps: true
-});
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("group", groupSchema);
