1e15c6b6299743bd61f4d6af6c34a07babd99fad
Jintae Song <w.jinttao@gmail.com>
2024-11-09 10:38:10 +0900

    Refactor: userController validaion 조건으로 isBlank 추가

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,18 +1,18 @@
 const keywordModel = require("../models/keywordModel");
 const userModel = require("../models/userModel");
-const { isValidString } = require("../utils/validation");
+const { isValidString, isBlank } = require("../utils/validation");
 
 const create = async (req, res) => {
-  if (!isValidString(req.body.uid)) {
+  if (!isValidString(req.body.uid) || isBlank(req.body.uid)) {
     return res.status(400).send({ message: "[InvalidUid] Error occured" });
   }
-  if (!isValidString(req.body.email)) {
+  if (!isValidString(req.body.email) || isBlank(req.body.email)) {
     return res.status(400).send({ message: "[InvalidEmail] Error occured" });
   }
-  if (!isValidString(req.body.displayName)) {
+  if (!isValidString(req.body.displayName) || isBlank(req.body.displayName)) {
     return res.status(400).send({ message: "[InvalidDisplayName] Error occured" });
   }
-  if (!isValidString(req.body.photoURL)) {
+  if (!isValidString(req.body.photoURL) || isBlank(req.body.photoURL)) {
     return res.status(400).send({ message: "[InvalidPhotoURL] Error occured" });
   }
 
