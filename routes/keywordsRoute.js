43c9d2b81628e995cd74fe1eeafd1307a9e2b922
Jintae Song <w.jinttao@gmail.com>
2024-11-11 16:07:51 +0900

    Feat: keywords 라우터 설정

--- routes/keywordsRoute.js
+++ routes/keywordsRoute.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const keywordsController = require("../controllers/keywordsController");
 
 router.get("/:keywordId", keywordsController.list);
+router.put("/:keywordId", keywordsController.put);
 
 module.exports = router;
