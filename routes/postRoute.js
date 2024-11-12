aef40686b82b017b2415bb8685c210d6a7493c50
HANSOL <hansol302@gmail.com>
2024-11-12 14:54:05 +0900

    Feat: 주간 게시물 수 조회 API  구현

--- routes/postRoute.js
+++ routes/postRoute.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const postController = require("../controllers/postController");
 
 router.get("/:keywordId/today", postController.today);
+router.get("/:keywordId/postCount", postController.postCount);
 router.get("/:keywordId", postController.list);
 
 module.exports = router;
