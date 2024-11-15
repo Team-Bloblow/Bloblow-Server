532cc5eb9a962082f3a1da3cb0d77dec41014880
Kyukyoung Song (James Song) <skdmlrh12@gmail.com>
2024-11-15 11:49:58 +0900

    Fix: setHours 밀리초에 대한 대응

--- controllers/postController.js
+++ controllers/postController.js
@@ -316,8 +316,9 @@ const groupPostCount = async (req, res) => {
   if (isValidString(req.query.cursorId)) {
     if (isEmptyString(req.query.cursorId)) {
       cursorIdDate = new Date();
-      cursorIdDate.setHours(0, 0, 0, 0);
       cursorIdDate.setDate(cursorIdDate.getDate() - 1 - cursorIdDate.getDay());
+      cursorIdDate.setHours(0, 0, 0, 0);
+      cursorIdDate = cursorIdDate.toISOString();
     } else {
       cursorIdDate = req.query.cursorId;
     }
@@ -460,8 +461,9 @@ const groupLikeCount = async (req, res) => {
   if (isValidString(req.query.cursorId)) {
     if (isEmptyString(req.query.cursorId)) {
       cursorIdDate = new Date();
-      cursorIdDate.setHours(0, 0, 0, 0);
       cursorIdDate.setDate(cursorIdDate.getDate() - 1 - cursorIdDate.getDay());
+      cursorIdDate.setHours(0, 0, 0, 0);
+      cursorIdDate = cursorIdDate.toISOString();
     } else {
       cursorIdDate = req.query.cursorId;
     }
@@ -604,8 +606,9 @@ const groupCommentCount = async (req, res) => {
   if (isValidString(req.query.cursorId)) {
     if (isEmptyString(req.query.cursorId)) {
       cursorIdDate = new Date();
-      cursorIdDate.setHours(0, 0, 0, 0);
       cursorIdDate.setDate(cursorIdDate.getDate() - 1 - cursorIdDate.getDay());
+      cursorIdDate.setHours(0, 0, 0, 0);
+      cursorIdDate = cursorIdDate.toISOString();
     } else {
       cursorIdDate = req.query.cursorId;
     }
