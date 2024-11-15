f3ba0e7f75dbe47584178d9665e9bf58e8314b92
Kyukyoung Song <ssongq92@gmail.com>
2024-11-15 13:43:09 +0900

    Feat: 그룹 대시보드 내 모든 키워드에 대해서 주간 게시물 수, 좋아요 수, 댓글 수 조회 API (#32)
    * Feat: 그룹 내 모든 키워드에 대한 주간 게시물 수 조회 API 구현
    
    * Feat: 커서 아이디를 포함하여 보낼 때 구현
    
    * Fix: 전달한 nextCursorId, previousCursorId를 넣어서 보내는 시나리오 구현
    
    * Feat: 좋아요 및 댓글 수 정보 포함 구현
    
    * Fix: 일자가 1의 자리일 경우에 대한 디버깅
    
    * Fix: 달이 1의 자리일 경우에 대한 디버깅
    
    * Feat: 그룹 내 모든 키워드에 대한 주간 게시물 수 조회 API 구현
    
    * Feat: 커서 아이디를 포함하여 보낼 때 구현
    
    * Fix: 전달한 nextCursorId, previousCursorId를 넣어서 보내는 시나리오 구현
    
    * Feat: 좋아요 및 댓글 수 정보 포함 구현
    
    * Fix: 일자가 1의 자리일 경우에 대한 디버깅
    
    * Fix: 달이 1의 자리일 경우에 대한 디버깅
    
    * Refactor: 그룹 차트 타입이 구별됨에 따라 로직 수정
    
    * Feat: dateToString에 timezone 옵션 추가
    
    * Fix: setHours 밀리초에 대한 대응
    
    * Feat: 그룹 내 모든 키워드에 대한 주간 게시물 수 조회 API 구현
    
    * Feat: 커서 아이디를 포함하여 보낼 때 구현
    
    * Fix: 전달한 nextCursorId, previousCursorId를 넣어서 보내는 시나리오 구현
    
    * Feat: 좋아요 및 댓글 수 정보 포함 구현
    
    * Fix: 일자가 1의 자리일 경우에 대한 디버깅
    
    * Fix: 달이 1의 자리일 경우에 대한 디버깅
    
    * Feat: 커서 아이디를 포함하여 보낼 때 구현
    
    * Fix: 전달한 nextCursorId, previousCursorId를 넣어서 보내는 시나리오 구현
    
    * Feat: 좋아요 및 댓글 수 정보 포함 구현
    
    * Refactor: 그룹 차트 타입이 구별됨에 따라 로직 수정
    
    * Feat: dateToString에 timezone 옵션 추가
    
    * Fix: setHours 밀리초에 대한 대응
    
    * Fix: 머지 충돌 해결
    
    * Refactor: 시간 포함 로직으로 수정
    
    * Chore: prettier 적용

--- routes/postRoute.js
+++ routes/postRoute.js
@@ -7,5 +7,8 @@ router.get("/keywords/:keywordId/postCount", postController.postCount);
 router.get("/keywords/:keywordId/postLike", postController.postLike);
 router.get("/keywords/:keywordId/postComment", postController.postComment);
 router.get("/:keywordId", postController.list);
+router.get("/groups/:groupId/postCount", postController.groupPostCount);
+router.get("/groups/:groupId/likeCount", postController.groupLikeCount);
+router.get("/groups/:groupId/commentCount", postController.groupCommentCount);
 
 module.exports = router;
