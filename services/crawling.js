2c82c5ec04916fb7353276294cffaa3d0e547050
Kyukyoung Song (James Song) <skdmlrh12@gmail.com>
2024-11-16 23:25:48 +0900

    Refactor: 서버에서 puppeteer 실행을 위한 세팅 - 3

--- services/crawling.js
+++ services/crawling.js
@@ -13,7 +13,7 @@ const NAVER_CLIENT_SECRET = process.env.NAVER_CLIENT_SECRET;
 const getPostCrawlingData = async (post) => {
   const browser = await puppeteer.launch({
     headless: true,
-    args: ["--no-sandbox", "--disable-setuid-sandbox", "--disable-dev-shm-usage", "--disable-gpu"],
+    args: ["--no-sandbox", "--disable-setuid-sandbox", "--disable-dev-shm-usage", "--disable-gpu", "--single-process", "--no-zygote"],
     ignoreHTTPSErrors: true,
   });
   try {
