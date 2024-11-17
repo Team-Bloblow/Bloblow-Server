7bf3c48f8420482ac3c234959dcebcb5374b70e3
Ssong-Q <skdmlrh12@gmail.com>
2024-11-17 09:52:52 +0900

    Refactor: 쿼리 스트링으로 변경

--- utils/fetchHandler.js
+++ utils/fetchHandler.js
@@ -1,9 +1,12 @@
-const fetchHandler = async ({ url, method = "GET" }) => {
+const fetchHandler = async ({ url, method = "GET", params }) => {
+  const queryString = new URLSearchParams(params).toString();
+  const fullUrl = queryString ? `${url}?${queryString}` : `${url}`;
+
   const options = {
     method,
   };
 
-  const response = await fetch(url, options);
+  const response = await fetch(fullUrl, options);
 
   if (!response.ok) {
     console.error(response.status);
