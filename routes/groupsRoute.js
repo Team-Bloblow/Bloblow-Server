20369e3f910526722c926710dd322a3d70e97b66
Jintae Song <w.jinttao@gmail.com>
2024-11-20 15:39:05 +0900

    Refactor: 중복 로직 수정

--- routes/groupsRoute.js
+++ routes/groupsRoute.js
@@ -3,6 +3,5 @@ const router = express.Router();
 const groupsController = require("../controllers/groupsController");
 
 router.get("/:uid", groupsController.list);
-router.put("/:groupId", groupsController.edit);
 
 module.exports = router;
