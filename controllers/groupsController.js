e70615791a3aa200acc55eaedd7da641b66cf482
Jintae Song <w.jinttao@gmail.com>
2024-12-27 01:54:28 +0900

    Feat: 유저 게시물 중 최근 생성일자 연산 로직 구현

--- controllers/groupsController.js
+++ controllers/groupsController.js
@@ -1,4 +1,6 @@
 const groupModel = require("../models/groupModel");
+const keywordModel = require("../models/keywordModel");
+const postModel = require("../models/postModel");
 const { isValidString, isEmptyString } = require("../utils/validation");
 
 const list = async (req, res) => {
@@ -20,4 +22,33 @@ const list = async (req, res) => {
   }
 };
 
-module.exports = { list };
+const summary = async (req, res) => {
+  if (!isValidString(req.params.uid) || isEmptyString(req.params.uid)) {
+    return res.status(400).send({ message: "[InvalidUid] Error occured" });
+  }
+
+  try {
+    const { uid } = req.params;
+    const keywordListResult = await keywordModel.find({ ownerUid: uid }).exec();
+    const keywordIdList = keywordListResult.map((keyword) => keyword._id);
+    const postCreatedLast = [];
+
+    for await (const id of keywordIdList) {
+      const postResult = await postModel.find({ keywordId: id }).sort({ createdAt: -1 }).exec();
+      postCreatedLast.push({
+        keywordId: postResult[0].keywordId,
+        createdAt: postResult[0].createdAt,
+      });
+    }
+
+    const postCreatedNewest = postCreatedLast?.reduce((prev, curr) => {
+      return new Date(prev.createdAt) <= new Date(curr.createdAt) ? curr : prev;
+    });
+  } catch {
+    return res
+      .status(500)
+      .send({ message: "[ServerError] Error occured in 'groupsController.summary'" });
+  }
+};
+
+module.exports = { list, summary };
