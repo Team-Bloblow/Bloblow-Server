756e023c75d618e60ba80266be987aeb1d4c4129
HANSOL <hansol302@gmail.com>
2024-12-26 18:20:44 +0900

    Feat: 키워드에 대한 게시물 수동 크롤링 로직 구현

--- controllers/keywordsController.js
+++ controllers/keywordsController.js
@@ -1,6 +1,7 @@
 const { isValidString, isEmptyString } = require("../utils/validation");
 const keywordModel = require("../models/keywordModel");
 const postModel = require("../models/postModel");
+const { getKeywordPostList } = require("../services/crawling");
 
 const list = async (req, res) => {
   if (!isValidString(req.params.keywordId) || isEmptyString(req.params.keywordId)) {
@@ -22,4 +23,26 @@ const list = async (req, res) => {
   }
 };
 
-module.exports = { list };
+const update = async (req, res) => {
+  if (!isValidString(req.params.keywordId) || isEmptyString(req.params.keywordId)) {
+    return res.status(400).send({ message: "[InvalidKeywordId] Error occured" });
+  }
+
+  const keywordInfo = await keywordModel.findById(req.params.keywordId).exec();
+  if (keywordInfo === null) {
+    return res.status(400).send({ message: "[NotExistedKeywordId] Error occured" });
+  }
+
+  const keywordId = req.params.keywordId;
+
+  try {
+    await getKeywordPostList(keywordInfo.keyword, keywordId);
+    return res.status(200).json({ status: "ok" });
+  } catch {
+    return res
+      .status(500)
+      .send({ message: "[ServerError] Error occured in 'keywordsController.update'" });
+  }
+};
+
+module.exports = { list, update };
