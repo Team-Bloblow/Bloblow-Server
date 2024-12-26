fd28ecc4cd302bcf7e5f04906642ea04d3d5c2ea
only-pine <65760535+only-pine@users.noreply.github.com>
2024-12-26 20:21:26 +0900

    Feat: 키워드에 대한 게시물 수동 크롤링 로직 구현 (#66)

--- routes/keywordsRoute.js
+++ routes/keywordsRoute.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const keywordsController = require("../controllers/keywordsController");
 
 router.get("/:keywordId", keywordsController.list);
+router.post("/:keywordId", keywordsController.update);
 
 module.exports = router;
