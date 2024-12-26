8d1aefd8d9883c13e2f84534f7d7c66e8e374f50
HANSOL <hansol302@gmail.com>
2024-12-26 15:47:58 +0900

    Remove: indexRoute 파일 삭제 및 해당 파일 import 코드 삭제

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const cors = require("cors");
 
-const indexRouter = require("./routes/indexRoute");
 const userRouter = require("./routes/userRoute");
 const groupRouter = require("./routes/groupRoute");
 const groupsRouter = require("./routes/groupsRoute");
@@ -32,7 +31,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use("/", indexRouter);
 app.use("/user", userRouter);
 app.use("/group", groupRouter);
 app.use("/groups", groupsRouter);
